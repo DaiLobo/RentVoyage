88aee653a25ee29a22eeffe6e33f42ef847f9dba
DaiLobo <diana45637@live.com>
2024-11-09 20:40:43 -0300

    :sparkles: feat: adiciona sistema de tradução na página 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,17 @@
+import { GetStaticProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
 export default function Custom404() {
   return <h1>404 - Page Not Found</h1>;
 }
+
+export const getStaticProps: GetStaticProps = async ({ locale }) => {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale ?? "pt", [
+        "common",
+        "login",
+      ]))
+    }
+  };
+};
